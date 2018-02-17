8e069384eb65624dacb1ffeb3505ad9986cc291e
Marco van de Kamp <marcovdk@gmail.com>
2018-02-17 08:10:44 +0100

    test

---
+++ test/test.php
@@ -0,0 +1,11 @@
+<?php
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+/**
+ * Created by PhpStorm.
+ * User: hansveenendaal
+ * Date: 08-01-18
+ * Time: 19:39
+ */
+echo "t1";
