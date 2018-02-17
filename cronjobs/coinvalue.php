d87f480116841d7eaa5a03bf4203589b599bd9ef
Hans Veenendaal <hans@tracebuzz.com>
2018-02-17 20:11:25 +0100

    testje van hns

--- cronjobs/coinvalue.php
+++ cronjobs/coinvalue.php
@@ -3,7 +3,7 @@
 require "/var/www/coins/config.php";
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch,    CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Set curl to return the data instead of printing it to the browser.
