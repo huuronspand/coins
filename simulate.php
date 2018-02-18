1c418639ecc361696c2bba545f5fac4ed4df94fe
Hans Veenendaal <hans@tracebuzz.com>
2018-02-18 11:22:42 +0100

    sim

---
+++ simulate.php
@@ -0,0 +1,21 @@
+<?php
+
+$savings = 10000;
+$savingsInCoins = 0;
+$defaultBuyAmount = 1000;
+$top10 = array();
+$simStartTimestamp = 1518876000;
+$simDays = 10;
+
+function sellCoin(coinCode)
+{
+    /* out of top 10, more than 100 rise ......? */
+    return true;
+}
+
+function getTop10(timeStamp)
+{
+    $top10 = array();
+
+    return $top10;
+}
