f599e4ef9a181df518a1691c91e871b2c999ad9c
Marco van de Kamp <marcovdk@gmail.com>
2018-02-22 20:19:43 +0100

    wat dan

--- cronjobs/bittrexcoins.php
+++ cronjobs/bittrexcoins.php
@@ -11,30 +11,34 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Set curl to return the data inste
 curl_setopt($ch, CURLOPT_URL, "https://bittrex.com/api/v1.1/public/getcurrencies");
 $data = curl_exec($ch);
 curl_close($ch);
-
-$coindata = json_decode($data);
-error_reporting(E_ALL);
 try {
-    $db = new PDO('mysql:host='.$_GLOBALS['dbLocation'].';dbname='.$_GLOBALS['db'].';charset=utf8mb4', $_GLOBALS['dbUser'], $_GLOBALS['dbPass']);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    echo 'Not possible to connect to mysql: ',  $e->getMessage(), "\n";
-}
-echo "Start checking bittrex coins";
-
-foreach ($coindata->result as $coin)
-{
-
-    echo $coin[name]."-".$coin->name;
-    $statement = $db->prepare("INSERT IGNORE INTO coins(name,symbol,bittrex)
-        VALUES(?,?,?)");
-
+    $coindata = json_decode($data);
+    error_reporting(E_ALL);
     try {
-        echo "-";
-        $statement->execute(array($coin[name],$coin[symbol],1));
+        $db = new PDO('mysql:host='.$_GLOBALS['dbLocation'].';dbname='.$_GLOBALS['db'].';charset=utf8mb4', $_GLOBALS['dbUser'], $_GLOBALS['dbPass']);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (Exception $e) {
-        echo 'Insert into coins not working: ',  $e->getMessage(), "\n";
+        echo 'Not possible to connect to mysql: ',  $e->getMessage(), "\n";
     }
+    echo "Start checking bittrex coins";
+
+    foreach ($coindata->result as $coin)
+    {
+
+        echo $coin[name]."-".$coin->name;
+        $statement = $db->prepare("INSERT IGNORE INTO coins(name,symbol,bittrex)
+            VALUES(?,?,?)");
+
+        try {
+            echo "-";
+            $statement->execute(array($coin[name],$coin[symbol],1));
+        } catch (Exception $e) {
+            echo 'Insert into coins not working: ',  $e->getMessage(), "\n";
+        }
+    }
+
+} catch (Exception $e) {
+    echo 'Super error: ',  $e->getMessage(), "\n";
 }
 
 ?>
