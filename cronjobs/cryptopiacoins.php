070a025cfdcb116c10683365e32649f30685a68a
Marco van de Kamp <marcovdk@gmail.com>
2018-02-22 20:56:21 +0100

    t

--- cronjobs/cryptopiacoins.php
+++ cronjobs/cryptopiacoins.php
@@ -24,7 +24,8 @@ try {
 
     foreach ($coindata->Data as $coin)
     {
-
+    echo $coin->Name."aaa";
+    die();
         $statement = $db->prepare("INSERT INTO coins (coinName,coinSymbol,cryptopia) VALUES (:name, :symbol, :cryptopia1) on duplicate key update cryptopia=:cryptopia2");
 
         try {
